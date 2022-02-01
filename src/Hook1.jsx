1d28b5c68e913e3c7b8cd0d3704a8c603cd52cd5
Sobit Prasad <sobitp59@gmail.com>
2022-02-01 17:52:30 +0530

    Hooks at a Glance Completed

--- src/Hook1.jsx
+++ src/Hook1.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 function Hook1(){
 
@@ -7,12 +7,16 @@ function Hook1(){
     function handleCount(){
         return setCount(count + 1);
     }
+    useEffect(()=>{
+        document.title = `You have clicked ${count} times.`;
+    })
     return (
         <>
             <hr />
         <div className="hook1">
             <h2>useState Hook</h2>
-            <h3>You have clicked {count}times.</h3>
+            <h3>Hooks at a Glance</h3>
+            <h3>You have clicked {count} times.</h3>
             <button onClick={handleCount}>Click Me</button>
         </div>
         <hr />
@@ -21,3 +25,11 @@ function Hook1(){
 }
 
 export default Hook1;
+
+
+//Rules of HOOKS 🔥
+
+// Hooks are JavaScript functions, but they impose two additional rules:
+// Only call Hooks at the top level. Don’t call Hooks inside loops, conditions, or nested functions.
+// Only call Hooks from React function components. Don’t call Hooks from regular JavaScript functions. 
+// (There is just one other valid place to call Hooks — your own custom Hooks.
