16f51e0eca6a3d5864f5b4004c0f986171b2f313
Sobit Prasad <sobitp59@gmail.com>
2022-02-03 19:06:04 +0530

    learned useEffect

--- src/UseEffect.jsx
+++ src/UseEffect.jsx
@@ -2,14 +2,25 @@ import React, { useState, useEffect } from 'react';
 
 const UseEffect = () => {
     const [newMessage,setNewMessage] = useState(0);
+    const [windowWidth, setWindowWidth] = useState(window.screen.width);
+    const [name, setName] = useState('Sobit');
 
     function handleMessage(){
         setNewMessage(newMessage + 1);
     }
 
+    const actualScreenSize = () => {
+        setWindowWidth(window.innerWidth);
+    }
     useEffect(()=>{
         document.title = `You have ${newMessage} new msessages.`;
-    })
+        window.addEventListener('resize', actualScreenSize);
+        //cleanup function
+        return () => {
+            window.removeEventListener('resize', actualScreenSize);
+        }
+    },[name]);
+    // dependencies in useEffect
 
 
     return (
@@ -25,8 +36,10 @@ const UseEffect = () => {
                 {/* Effects with cleanup */}
                 <div>
                     <h2>Effects with CLEANUP</h2>
-                    <h3>Watch Thapa Technical Video From MXPlayer</h3>
+                    <h3>The actual size of the window is : {windowWidth}</h3>
                 </div>
+                <button onClick={() => setName('Rohit')}>Change Name</button>
+                <p>{name}</p>
             </div>
             <hr />
         </React.Fragment>
